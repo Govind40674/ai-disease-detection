4f14dadc1609a03f4462ff8ef54c01d80f9d5086
Govind40674 <govindkumar4067444@outlook.com>
2026-03-16 09:43:15 +0530

    change css in home page

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
-// import Header from '../../components/header/header'
+import React, { useEffect } from 'react'
 import Header from "../../components/header/Header";
 import First_page from '../../components/first_page/First_page'
 import Footer from '../../components/footer/Footer';
+import styles from './home.module.css'
 
 function Home() {
+
+  useEffect(() => {
+    document.body.classList.add("home-body")
+
+    return () => {
+      document.body.classList.remove("home-body")
+    }
+  }, [])
+
   return (
     <>
-    <Header/>
-    <First_page/>
-    <Footer/>
+      <Header/>
+      <First_page/>
+      <Footer/>
     </>
   )
 }
